3d3712ad53a3b77f19bca799b1dcafe54dbb15e3
Viktor Lundberg <vikkolundberg@gmail.com>
2025-12-10 09:30:18 +0100

    feat: boilerplate code with tanstack and clerk

---
+++ src/routes/__root.tsx
@@ -0,0 +1,38 @@
+import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { TanStackDevtools } from '@tanstack/react-devtools'
+
+import Header from '../components/Header'
+
+import ClerkProvider from '../integrations/clerk/provider'
+
+import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
+
+import type { QueryClient } from '@tanstack/react-query'
+
+interface MyRouterContext {
+  queryClient: QueryClient
+}
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
+  component: () => (
+    <>
+      <ClerkProvider>
+        <Header />
+        <Outlet />
+        <TanStackDevtools
+          config={{
+            position: 'bottom-right',
+          }}
+          plugins={[
+            {
+              name: 'Tanstack Router',
+              render: <TanStackRouterDevtoolsPanel />,
+            },
+            TanStackQueryDevtools,
+          ]}
+        />
+      </ClerkProvider>
+    </>
+  ),
+})
