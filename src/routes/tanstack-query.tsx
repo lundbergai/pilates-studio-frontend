ad955c368254738cad922300bf8df256c690a5e0
Viktor Lundberg <vikkolundberg@gmail.com>
2025-12-11 09:29:27 +0100

    chore: remove tanstack query demo

--- src/routes/tanstack-query.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import TanStackQueryDemo from "../components/TanStackQueryDemo";
-
-export const Route = createFileRoute("/tanstack-query")({
-	component: TanStackQueryDemo
-});
